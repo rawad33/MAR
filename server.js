219d5129cdb93afc352f301ab1a9e9d4032d2b1e
rawad <rawad.sabik@gmail/com>
2020-08-17 14:20:18 +0300

    a

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.use(express.static('public'));
 
+
 const port = process.env.PORT || 4000;
-app.listen(port,()=>{console.log(' listen on port', port)})
-;
+app.listen(port,()=>{console.log(' listen on port', port)});
