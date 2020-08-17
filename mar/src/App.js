fb87954df0cb8ba21cb63d4cd0bd362f0151f255
rawad <rawad.sabik@gmail/com>
2020-08-17 14:48:48 +0300

    for you

--- mar/src/App.js
+++ mar/src/App.js
@@ -2,25 +2,71 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useHistory
+} from "react-router-dom";
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">login</Link>
+            </li>
+            <li>
+              <Link to="/about">Login</Link>
+            </li>
+            
+       
+          </ul>
+        </nav>
+
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/register">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  
+    </div>
+  );
+}
+function Home() {
+  let history =useHistory();
+  return(
+    <div className="login">
+      <h1>hello rege</h1>
+      <button onClick={()=>{
+        history.push('/register')
+      }}></button>
     </div>
   );
 }
 
+function About() {
+  return(
+<h1>12312</h1>
+  );
+}
+
+function Users() {
+  return <h2>register</h2>;
+}
 export default App;
