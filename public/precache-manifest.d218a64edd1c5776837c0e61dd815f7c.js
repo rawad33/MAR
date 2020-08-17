fc3f7e8a75fe9d2abdff2fcff93b056f105706f4
rawad <rawad.sabik@gmail/com>
2020-08-17 14:00:50 +0300

    react

---
+++ public/precache-manifest.d218a64edd1c5776837c0e61dd815f7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e50ff2382c54761a874cf4ac84aede34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81037f20256630456272",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d8eec93190aa7f72ac60",
+    "url": "/static/js/2.c8473e9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c8473e9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81037f20256630456272",
+    "url": "/static/js/main.11e1a7f2.chunk.js"
+  },
+  {
+    "revision": "49780edbf2b83fac2e2f",
+    "url": "/static/js/runtime-main.a59e9f04.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
