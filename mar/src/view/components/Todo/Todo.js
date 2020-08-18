5a472ee5f90ab12ca525fcf26b6ab6c8dfcf811c
abdalla <abdalla.b.94@gmail.com>
2020-08-18 15:25:21 +0300

    sof sof

--- mar/src/view/components/Todo/Todo.js
+++ mar/src/view/components/Todo/Todo.js
@@ -1,49 +1,79 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Todo.css'
+import Trash from './img/trash.png'
 
 
 
 
 function Todo() {
-  const [todotasks , setTodotasks] = useState([]);
+  const [todotasks, setTodotasks] = useState([]);
   let lastName = localStorage.getItem("lastName");
-  // console.log(lastName);
   console.log(todotasks[0])
-  
-  return (
-    <div className="todo">
-      <h1>TO-DO</h1>
-      <button onClick={ongetToDo}> get data</button>
-  
-  
- 
 
-    </div>
-
-  )
-  
-
-
- 
-   function ongetToDo() {
-    fetch('/api/todo', {  
+  useEffect(() => {
+    fetch('/api/todo', {
       method: 'POST',
       body: JSON.stringify({ lastName }),
       headers: {
         'Content-Type': 'application/json'
       }
-  
-    })
-    .then(res => res.json())
-    .then(data => {
-      // console.log(data)
-      // console.log(data[0])
-      // console.log(data[0]._id)
-      setTodotasks(data);
-
 
     })
-   }
+      .then(res => res.json())
+      .then(data => {
+        // console.log(data)
+        // console.log(data[0])
+        // console.log(data[0]._id)
+        setTodotasks(data);
+
+
+      })
+
+  }, [])
+
+  return (
+    <div className="todo">
+
+      <div id="tasks">
+        {
+          todotasks.map((elem, index) => {
+            return (
+
+              <div>
+                <p >{elem._id}</p>
+                {
+
+                  elem.tasks.map((task, index) => {
+                    return (
+                      <div>
+                             <p> {index+1} {task.taskTitle} </p>
+                            <button>sad</button>
+                            <img src={Trash} alt="trash"  />
+                              <input type="checkbox" />       
+                       </div>
+                              
+                    )
+                                
+                              })}
+                  
+              </div>
+
+            )
+          })
+        }
+      </div>
+    </div>
+
+
+
+  )
+
+
+
+
+  //  function ongetToDo() {
+
+  //  }
 
 }
 
